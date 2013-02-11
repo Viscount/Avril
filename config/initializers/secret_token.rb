bd2e84e9be5d50d3d160627d77aad9a4f7061221
廖振宇 <maple01@126.com>
2013-02-11 23:18:43 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avril::Application.config.secret_token = 'b2923da4445e2796351b1b104324b7e9fcc9575c3c7ed32f7b1cf45c766f29f06a1c4da26b34066e37497a43baa83789abe0ad777b9c3c991093d5cdc6bd76cc'
