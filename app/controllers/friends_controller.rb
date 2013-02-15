abc342d562ae8e41cc92f824b38b6757864be22a
greenmoon55 <greenmoon55@gmail.com>
2013-02-15 21:35:17 +0800

    全部关注的用户的地区分析

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,21 +1,26 @@
 class FriendsController < ApplicationController
   def index
     @province_hash = get_province
-    uri = URI("https://api.weibo.com/2/friendships/friends.json?access_token=#{session['access_token']}&screen_name=greenmoon55&count=200") 
+    cursor = 0
+    @counter = 0  # 总人数
+    @h = Hash.new
+    begin
+      res = get_friends(cursor)
+      cursor += 200
+      analyze(res)
+    end while res["next_cursor"] > 0
+    return @res = @h
+  end
+
+  def get_friends(cursor = 0)
+    uri = URI("https://api.weibo.com/2/friendships/friends.json?access_token=#{session['access_token']}&screen_name=greenmoon55&count=200&cursor=#{cursor}") 
+    res = ''
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       request = Net::HTTP::Get.new uri.request_uri
       response = http.request request
       res = JSON.parse(response.body)
-      @res = Hash.new
-      # 这里写得好丑= =!
-      res["users"].each do |user|
-        if @res[get_name(user["province"])].nil?
-          @res[get_name(user["province"])] = 1
-        else
-          @res[get_name(user["province"])] += 1
-        end
-      end
     end
+    return res
   end
 
   # 获取省份id对应名字的列表
@@ -36,5 +41,17 @@ class FriendsController < ApplicationController
   def get_name(raw_id)
     new_id = "0010" + raw_id
     @province_hash[new_id]
+  end
+
+  def analyze(res)
+    # 这里写得好丑= =!
+    res["users"].each do |user|
+      if @h[get_name(user["province"])].nil?
+        @h[get_name(user["province"])] = 1
+      else
+        @h[get_name(user["province"])] += 1
+      end
+      @counter += 1
+    end
   end
 end
